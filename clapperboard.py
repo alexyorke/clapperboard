852c19e0ec0dfbbc3141f1e3230505a6e1e20625
Decagon <Decagon@users.noreply.github.com>
2017-01-08 18:23:07 -0400

    Add support for malformed AAC bitstreams

--- clapperboard.py
+++ clapperboard.py
@@ -20,12 +20,12 @@ offset = 0
 # will only work for two or three audio channels
 print "Recognized audio channel starting positions: " + str(audioChannels)
 if len(audioChannels) == 2:
-    offset = str(float(audioChannels[0]) - float(audioChannels[1])) + "s"
-    print "Offset: " + offset
+    offset = str(float(audioChannels[0]) - float(audioChannels[1]))
+    print "Offset: " + offset + "s"
 elif len(audioChannels) == 3:
     avgTwoAndThree = (float(audioChannels[1]) + float(audioChannels[2]))/2.0
-    offset = str(float(audioChannels[0]) - avgTwoAndThree) + "s"
-    print "Offset: " + offset
+    offset = str(float(audioChannels[0]) - avgTwoAndThree)
+    print "Offset: " + offset + "s"
 else:
     print "Audio offset could not be determined."
     exit()
@@ -35,10 +35,10 @@ if offset == 0:
     exit()
 
 # prepare conversion
-output = subprocess.Popen(["ffmpeg -i '" + inputFile + " -itsoffset " +
+output = subprocess.Popen(["ffmpeg -i '" + inputFile + "' -itsoffset " +
                            offset + " -i '" + inputFile + "' -map 0:v " +
-                           " -map 1:a -vcodec copy" +
+                           " -bsf:a aac_adtstoasc -map 1:a -vcodec copy" +
                            " -acodec copy '" + outputFile + "'"],
                           stdout=subprocess.PIPE, shell=True).communicate()[0]
 
-print "Converted file."
+print "Converted file (check ffmpeg status.)"
