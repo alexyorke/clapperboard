fc52db3e37402e9765209239818dabf2f7865226
Decagon <Decagon@users.noreply.github.com>
2017-01-08 18:20:49 -0400

    Add output-to-file feature

--- clapperboard.py
+++ clapperboard.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 
 inputFile = sys.argv[1]
+outputFile = sys.argv[2]
 
 # prepare command
 output = subprocess.Popen(["ffprobe -v quiet -show_format -show_streams '" +
@@ -15,6 +16,7 @@ audioChannels = output.split("\n")
 # last line is always empty
 audioChannels.pop()
 
+offset = 0
 # will only work for two or three audio channels
 print "Recognized audio channel starting positions: " + str(audioChannels)
 if len(audioChannels) == 2:
@@ -26,3 +28,17 @@ elif len(audioChannels) == 3:
     print "Offset: " + offset
 else:
     print "Audio offset could not be determined."
+    exit()
+
+if offset == 0:
+    print "No audio offset."
+    exit()
+
+# prepare conversion
+output = subprocess.Popen(["ffmpeg -i '" + inputFile + " -itsoffset " +
+                           offset + " -i '" + inputFile + "' -map 0:v " +
+                           " -map 1:a -vcodec copy" +
+                           " -acodec copy '" + outputFile + "'"],
+                          stdout=subprocess.PIPE, shell=True).communicate()[0]
+
+print "Converted file."
